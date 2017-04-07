430dfb45f887c86beac2aca2f55117993fd83820
Rahadian Yusuf <rahadian.y@gmail.com>
2017-04-07 19:42:48 +0700

    add new test (CrepeStore.state)

--- test/test.js
+++ test/test.js
@@ -10,4 +10,29 @@ describe('CrepeStore', function() {
       assert.deepEqual(store.state, {});
     })
   });
+
+  describe('#state', function() {
+    let state = {
+      a: 1,
+      b: 2
+    };
+
+    let store = new CrepeStore({ state: state });
+
+    it('contains same state as the one passed in constructor parameters', function() {
+      assert.deepEqual(store.state.a, state.a);
+      assert.deepEqual(store.state.b, state.b);
+    });
+
+    it('cannot change value by assignment', function() {
+      try {
+        store.state.a = 10;
+      } catch(e) {
+        // ignore error -_-
+        // We just care whether the value is changed
+      }
+      assert.notEqual(store.state.a, 10);
+      assert.equal(store.state.a, 1);
+    });
+  });
 });
