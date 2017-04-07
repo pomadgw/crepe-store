8d01cac75abf6b9bc809b19b4d8cb59542cdca5b
Rahadian Yusuf <rahadian.y@gmail.com>
2017-04-07 13:39:14 +0700

    Change content for browser

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-(function (factory) {
-    // The below code are based from https://github.com/melanke/Watch.JS
-
-    if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD.
-        define('CrepeStore', [], factory);
-    } else {
-        // Browser globals
-        window.CrepeStore = factory();
-    }
-})(require('./crepestore'));
+window.CrepeStore = require('./crepestore').default;
