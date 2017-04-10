ffabc1245612cfb35c2b3b32832e5ebbac0a8d16
Rahadian Yusuf <rahadian.y@gmail.com>
2017-04-10 20:16:48 +0700

    modify test & add test for add mutation

--- test/test.js
+++ test/test.js
@@ -20,19 +20,47 @@ describe('CrepeStore', function() {
     let store = new CrepeStore({ state: state });
 
     it('contains same state as the one passed in constructor parameters', function() {
-      assert.deepEqual(store.state.a, state.a);
-      assert.deepEqual(store.state.b, state.b);
+      let stateStoreKey = Object.getOwnPropertyNames(store.state);
+      let realStateKey = Object.getOwnPropertyNames(state);
+      assert.deepEqual(stateStoreKey, realStateKey);
     });
 
     it('cannot change value by assignment', function() {
+      const newVal = 10;
+      const oldVal = store.state.a;
       try {
-        store.state.a = 10;
+        store.state.a = newVal;
       } catch(e) {
         // ignore error -_-
         // We just care whether the value is changed
       }
-      assert.notEqual(store.state.a, 10);
-      assert.equal(store.state.a, 1);
+      assert.notEqual(store.state.a, newVal);
+      assert.equal(store.state.a, oldVal);
+    });
+  });
+
+  describe('#addMutation', function() {
+    it('can add mutation', function() {
+      let store = new CrepeStore({
+        state: {
+          a: 10
+        }
+      });
+
+      const oldVal = store.state.a;
+      const newVal = 10;
+      const mutateObj = {
+        type: 'modifyA',
+        fn: (state) => {
+          state.a = newVal;
+        }
+      };
+
+      store.addMutation(mutateObj);
+      store.addAction({ type: 'check', fn: ({ commit }) => commit('modifyA') });
+      store.dispatch('check');
+
+      assert.equal(store.state.a, newVal);
     });
   });
 });
